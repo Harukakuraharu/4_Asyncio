61b7b338fd2811b11eab79b07849297e33c46386
Gulnaz Artykaeva <akhmetovagrm@gmail.com>
2025-01-15 02:38:45 +0500

    Added docstrings

--- utils/utils.py
+++ utils/utils.py
@@ -1,9 +1,9 @@
 import os
 from dataclasses import dataclass, field
-from typing import Any
 
 import aiohttp
 import pandas as pd
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.database import async_session
 from crud import crud
@@ -13,7 +13,9 @@ from crud import crud
 class FileDownloader:
     """Class for work with file - download and delete"""
 
-    async def initial_download(self, current_date):
+    async def initial_download(self, current_date: str) -> None:
+        """Initial download file and parsing for insert to database"""
+
         url = f"https://spimex.com/upload/reports/oil_xls/oil_xls_{current_date}162000.xls?r=9455"
         filename = f"{current_date}.xls"
         await self.download_file(url, filename)
@@ -24,7 +26,8 @@ class FileDownloader:
         except FileNotFoundError:
             pass
 
-    async def download_file(self, url, filename: str) -> tuple[str, Any]:
+    async def download_file(self, url, filename: str) -> None:
+        """Download file"""
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as response:
                 if response.status == 200:
@@ -33,22 +36,26 @@ class FileDownloader:
                         f.write(content)
 
     def delete_file(self, filename: str) -> None:
+        """Delete file"""
         if os.path.exists(filename):
             os.remove(filename)
 
 
 @dataclass
 class FileParser:
+    """Class for parsing downloaded files"""
 
     filename: str
-    data: list = field(default_factory=pd.DataFrame)
-    async_session = async_session
+    data: pd.DataFrame = field(default_factory=pd.DataFrame)
+    async_session: AsyncSession = async_session
 
-    async def get_data(self, filename):
+    async def get_data(self, filename: str) -> pd.DataFrame:
+        """Get data like pandas dataframe"""
         data = pd.read_excel(filename)
         return data
 
-    async def update_table(self):
+    async def update_table(self) -> str:
+        """Update data before insert in to database"""
         self.data = await self.get_data(self.filename)
         date_data = self.data["Форма СЭТ-БТ"].iloc[2].split(": ")
         self.data.columns = list(range(15))
@@ -57,6 +64,7 @@ class FileParser:
         return date_data[-1]
 
     async def make_data_list(self, date_data: str) -> list:
+        """Get list with data for insert in to database"""
         return [
             {
                 "exchange_product_id": row[1],
@@ -74,6 +82,7 @@ class FileParser:
         ]
 
     async def insert_to_db(self) -> None:
+        """Insert data to database"""
         async with self.async_session() as session:
             date_current = await self.update_table()
             data = await self.make_data_list(date_current)
